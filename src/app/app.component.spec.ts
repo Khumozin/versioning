45f7f52872ad12749d59dac15b09054ed7774de7
Khumo Mogorosi <KMogorosi@toyota.co.za>
2023-03-02 14:27:52 +0200

    test(app.component): unit test app componnet

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,14 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -26,10 +24,19 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Versioning');
   });
 
-  it('should render title', () => {
+  it(`should contain a version number`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.version).not.toEqual('');
+  });
+
+  it('should render text', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Versioning app is running!');
+
+    expect(compiled.querySelector('.mt-4')?.textContent).toContain(
+      'Documentation coming soon..'
+    );
   });
 });
